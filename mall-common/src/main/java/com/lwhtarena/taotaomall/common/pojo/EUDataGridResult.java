e58cc3d36ac6ced7e2f71702e4f80970dabf02f5
lwhtarena <lwhtarena@163.com>
2016-12-04 01:22:57 +0800

    ssm 框架搭建基本完成

--- mall-common/src/main/java/com/lwhtarena/taotaomall/common/pojo/EUDataGridResult.java
+++ mall-common/src/main/java/com/lwhtarena/taotaomall/common/pojo/EUDataGridResult.java
@@ -7,6 +7,8 @@ import java.util.List;
  * @folder com.lwhtarena.taotaomall.common.pojo
  * @date 2016/12/3.
  * @版权 Copyright (c) 2016 lwhtarena.com. All Rights Reserved.
+ * <p>接收分页参数，一个是page一个是rows。调用dao查询商品列表。并分页。返回商品列表。
+ * 返回一个EasyUIDateGrid支持的数据格式。封装成easyui的datagrid值，便于传参</p>
  */
 public class EUDataGridResult {
     private long total;
